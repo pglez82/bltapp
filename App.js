6a20b980e9e4c0325ecfe37f9f2deadd720c7cff
pablo <pglez82@gmail.com>
2020-11-24 21:40:13 +0100

    adding the ws integration

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Text, View ,Button} from 'react-native';
 import * as Location from 'expo-location';
 import { BleManager } from 'react-native-ble-plx';
+import uuid from 'react-native-uuid';
+
 
 class App extends React.Component {
   constructor(props){
@@ -9,7 +11,9 @@ class App extends React.Component {
     this.state = {textgps:"Waiting for position...",
                   bltscanning:false,
                   gotlocation:false,
-                  deviceIds:[]}
+                  deviceIds:[],
+                  wsencounters:[],
+                  uniqueId:""}
     this.manager = new BleManager()
   }
 
@@ -24,6 +28,12 @@ class App extends React.Component {
     this.setState({longitude:location['coords']['longitude']});
     if (location!=null)
       this.setState({gotlocation:true})
+    this.setState({uniqueId:uuid.v1()})
+  }
+
+  componentWillUnmount(){
+    if (this.state.bltscanning)
+      this.manager.stopDeviceScan();
   }
 
   findDevices(){
@@ -39,16 +49,41 @@ class App extends React.Component {
         if (error) { 
             return
         }
-        if (this.state.deviceIds.indexOf(device.id)==-1)
+        console.log("Device found")
+        if (this.state.deviceIds.indexOf(device.id)===-1){
+          this.saveInfo(this.state.uniqueId,device.id)
           this.setState({deviceIds: [...this.state.deviceIds, device.id]})
+        }
       });
-    }
+    } 
+  }
 
+  saveInfo(uniqueId,deviceId2){
+    console.log("Saving encounter to webservice")
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ deviceId1: uniqueId , deviceId2: deviceId2 })
+    };
+    //This is being executed in the phone so we need the address of the server. Change according to your settings
+    fetch('http://192.168.2.250:5000/api/encounters', requestOptions)
+        .catch( err => {
+          err.text().then( errorMessage => {
+            console.log(errorMessage)
+          })}
+        )}
+
+  async loadEncounters(){
+    const encodedId = encodeURIComponent(this.state.uniqueId)
+    const response = await fetch("http://192.168.2.250:5000/api/encounters/"+encodedId)
+    const encounters = await response.json();
+    this.setState({wsencounters:encounters['encounters']})
   }
 
   render(){
     return(
       <View>
+        <Text>UniqueID: {this.state.uniqueId}</Text>
         <Text>{this.state.textgps}</Text>
         <View>
           <Text>Latitude: {this.state.latitude}</Text>
@@ -62,6 +97,16 @@ class App extends React.Component {
           accessibilityLabel="Scan BLE devices"
         />
         {this.state.deviceIds.map(deviceId => {
+          return (
+              <Text key={deviceId}>{deviceId}</Text>
+          );
+        })}
+        <Button
+          onPress={this.loadEncounters.bind(this)}
+          title="LOAD LOGGED ENCOUNTERS FROM WS"
+          color="#841584"
+        />
+        {this.state.wsencounters.map(deviceId => {
           return (
               <Text key={deviceId}>{deviceId}</Text>
           );
